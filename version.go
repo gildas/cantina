f31175b57bcad55d6f7d0011ed28bb95bee171b0
Gildas Cherruel <gildas@breizh.org>
2022-10-27 20:38:58 +0900

    Bumped to version 1.2.3

--- version.go
+++ version.go
@@ -12,7 +12,7 @@ var branch string
 var stamp string
 
 // VERSION is the version of this application
-var VERSION = "1.2.2"
+var VERSION = "1.2.3"
 
 // APP is the name of the application
 const APP = "cantina"
